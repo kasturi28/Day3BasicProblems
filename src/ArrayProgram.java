735c14b4b36f10102f722dc31e789ef960a52483
kasturi28 <kasturipansare2818@gmail.com>
2022-07-03 12:18:37 +0530

    Added print_arrayelements_in_reverseorder

--- src/ArrayProgram.java
+++ src/ArrayProgram.java
@@ -82,5 +82,12 @@ public class ArrayProgram {
 		for (int i = 0; i < arr.length; i = i + 2) {
 			System.out.println(arr[i]);
 		}
+
+		System.out.println("Array in reverse order: ");
+		// Loop through the array in reverse order
+		for (int i = arr.length - 1; i >= 0; i--) {
+			System.out.print(arr[i] + " ");
+		}
+
 	}
 }
