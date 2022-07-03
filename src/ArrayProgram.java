979ccef1b3b37e92c7b748001216a73f0d61b171
kasturi28 <kasturipansare2818@gmail.com>
2022-07-03 12:38:29 +0530

    Added print_arrayelements_in_ascendingorder

--- src/ArrayProgram.java
+++ src/ArrayProgram.java
@@ -9,6 +9,7 @@ public class ArrayProgram {
 		 * initializing the array
 		 */
 		int[] arr = new int[] { 1, 2, 3, 4, 5, 1, 5, 2, 1, 3, 2, 2 };
+		int temp = 0;
 
 		System.out.println("Elements of given array: ");
 
@@ -16,9 +17,9 @@ public class ArrayProgram {
 
 		for (int i = 0; i < arr.length; i++) {
 
-			System.out.println(arr[i] + " ");
-
+			System.out.print(arr[i] + " ");
 		}
+		System.out.println();
 
 		// Array fr will store frequencies of element
 
@@ -56,6 +57,7 @@ public class ArrayProgram {
 				max = arr[i];
 		}
 		System.out.println("Largest element present in given array: " + max);
+		System.out.println();
 
 		// Initialize min with first element of array.
 		int min = arr[0];
@@ -65,27 +67,49 @@ public class ArrayProgram {
 			if (arr[i] < min)
 				min = arr[i];
 		}
+
 		System.out.println("Smallest element present in given array: " + min);
+		System.out.println();
 
 		System.out.println("Elements of given array present on even position: ");
 		// Loop through the array by incrementing value of i by 2
 		// Here, i will start from 1 as first even positioned element is present at
 		// position 1.
 		for (int i = 1; i < arr.length; i = i + 2) {
-			System.out.println(arr[i]);
+			System.out.print(arr[i] + " ");
 		}
+
+		System.out.println();
 
 		System.out.println("Elements of given array present on odd position: ");
 		// Loop through the array by incrementing value of i by 2
 		// Here, i will start from 1 as first even positioned element is present at
 		// position 1.
 		for (int i = 0; i < arr.length; i = i + 2) {
-			System.out.println(arr[i]);
+			System.out.print(arr[i] + " ");
 		}
+
+		System.out.println();
 
 		System.out.println("Array in reverse order: ");
 		// Loop through the array in reverse order
 		for (int i = arr.length - 1; i >= 0; i--) {
+			System.out.print(arr[i] + " ");
+		}
+		System.out.println();
+
+		// Sort the array in ascending order
+		System.out.println("Array in ascending order: ");
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = i + 1; j < arr.length; j++) {
+				if (arr[i] > arr[j]) {
+					temp = arr[i];
+					arr[i] = arr[j];
+					arr[j] = temp;
+				}
+			}
+		}
+		for (int i = 0; i < arr.length; i++) {
 			System.out.print(arr[i] + " ");
 		}
 
